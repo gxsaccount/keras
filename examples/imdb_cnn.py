4f7a478a499d53a72484460b34b42558ba49f532
Orlando Ding <xiandao.airs@gmail.com>
2018-04-28 23:22:07 +0800

    llv23 - for ladding orlando's CNN load

--- examples/imdb_cnn.py
+++ examples/imdb_cnn.py
@@ -1,10 +1,46 @@
+#!/usr/bin/env python -W ignore::DeprecationWarning
 '''This example demonstrates the use of Convolution1D for text classification.
 
 Gets to 0.89 test accuracy after 2 epochs.
 90s/epoch on Intel i5 2.4Ghz CPU.
-10s/epoch on Tesla K40 GPU.
+10s/epoch on Tesla K40 GPU - 12G Memory.
+
+References:
+    1. DeprecationWarning in tensorflow - https://stackoverflow.com/questions/879173/how-to-ignore-deprecation-warnings-in-python
+    2. Keras GPU/CPU model - https://stackoverflow.com/questions/40690598/can-keras-with-tensorflow-backend-be-forced-to-use-cpu-or-gpu-at-will/42750563#42750563
 '''
 from __future__ import print_function
+
+import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+import tensorflow as tf
+
+## 1. simple setup of default tensorflow session
+# tf_config = tf.ConfigProto(log_device_placement=True)
+# tf_config.gpu_options.allow_growth = True
+# tf_config.gpu_options.per_process_gpu_memory_fraction = 0.1
+# sess = tf.Session(config=tf_config)
+
+## 2. more strategy setup of default tensorflow session
+num_cores = 4
+GPU = len([v for v in os.environ["CUDA_VISIBLE_DEVICES"].split(',') if len(v)>0])
+
+if GPU > 0:
+    num_GPU = 1
+    num_CPU = 2
+else:
+    num_GPU = 0
+    num_CPU = 2
+
+tf_config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,\
+        inter_op_parallelism_threads=num_cores, allow_soft_placement=True,\
+        device_count = {'CPU' : num_CPU, 'GPU' : num_GPU})
+sess = tf.Session(config=tf_config)
+
+from keras import backend as K
+K.set_session(sess)
 
 from keras.preprocessing import sequence
 from keras.models import Sequential
@@ -15,7 +51,9 @@ from keras.datasets import imdb
 
 # set parameters:
 max_features = 5000
+# max_features = 50
 maxlen = 400
+# maxlen = 200
 batch_size = 32
 embedding_dims = 50
 filters = 250
